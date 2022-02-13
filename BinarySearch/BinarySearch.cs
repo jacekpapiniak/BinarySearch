2eb899dacda324a14d6651176cce9172eb8b709b
Jacek Papiniak <jacekpapiniak@gmail.com>
2022-02-13 13:43:22 +0000

    Binary search algorithm implementation

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,98 @@
+﻿namespace BinarySearchAlgorithm;
+
+public static class BinarySearch
+{
+    public static readonly List<string> SortedSureNames = new()
+    {
+        "Balaam",
+        "Bennett",
+        "Bone",
+        "Briggs",
+        "Brown",
+        "Bruce",
+        "Byrne",
+        "Chaudhry",
+        "Dempsey",
+        "Dodd",
+        "Findlay",
+        "Flynn",
+        "Forbes",
+        "Ford",
+        "Griffith",
+        "Harper",
+        "Homes",
+        "Johnston",
+        "Kaufman",
+        "Kelly",
+        "Lau",
+        "Martin",
+        "McDougall",
+        "McKenna",
+        "McMillan",
+        "Nicol",
+        "Noble", 
+        "Nugent",
+        "Parker",
+        "Pettigrew",
+        "Poyner",
+        "Roberts",
+        "Rogerson",
+        "Roose",
+        "Rose",
+        "Sahni",
+        "Singh",
+        "Smyth",
+        "Steer",
+        "Stewart",
+        "Tate",
+        "Taylor",
+        "Till",
+        "Twigg",
+        "Vincent",
+        "Ward",
+        "Warner",
+        "Wilkins",
+        "Winchester",
+        "Wright"
+    };
+
+    public static void Main()
+    {
+        var searchIndex0 = Search(SortedSureNames, 0, SortedSureNames.Count() - 1, "Balaam");
+        Console.WriteLine($"Balaam found at index: {searchIndex0}");
+        Console.WriteLine();
+        
+        var searchIndex1 = Search(SortedSureNames, 0, SortedSureNames.Count() - 1, "Parker");
+        Console.WriteLine($"Found searched Parker at index: {searchIndex1}");
+        Console.WriteLine();
+        
+        var searchIndex2 = Search(SortedSureNames, 0, SortedSureNames.Count() - 1, "Ros");
+        Console.WriteLine($"Ros do not exist in collection {searchIndex2}");
+        Console.WriteLine();
+        
+        var searchIndex3 = Search(SortedSureNames, 0, SortedSureNames.Count() - 1, "Wright");
+        Console.WriteLine($"Wright found on index {searchIndex3}");
+    }
+
+    public static int Search(List<string> data, int leftEnd, int rightEnd, string search)
+    {
+        if (leftEnd > rightEnd)
+        {
+            return -1;
+        }
+        
+        var mid = (leftEnd + rightEnd) / 2;
+        Console.WriteLine($"Current value: {data[mid]}, index: {mid}");
+
+        if (search == data[mid])
+        {
+            return mid;
+        }
+        else if (search.CompareTo(data[mid]) < 0)
+        {
+            return Search(data, leftEnd, mid-1, search);
+        }
+        
+        return Search(data,mid+1,rightEnd,search);
+    }
+}
---
+++ BinarySearchTests/BinarySearchTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using BinarySearchAlgorithm;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace BinarySearchTests;
+
+[TestFixture]
+public class BinarySearchTests
+{
+    private const int NotFoundIndex = -1;
+    private static readonly List<string> TestSureNames = BinarySearch.SortedSureNames;
+
+    [TestCase("Balaam", 0)]
+    [TestCase("Roose", 33)]
+    [TestCase("Homes", 16)]
+    [TestCase("Wright", 49)]
+    public void Search_ReturnIndex_WhenFoundItem(string search, int expectedIndex)
+        => BinarySearch.Search(TestSureNames, 0, TestSureNames.Count - 1, search).Should().Be(expectedIndex);
+    
+    [TestCase("Ros")]
+    [TestCase("Papiniak")]
+    public void Search_Return_Negative_One_WhenItem_NotFound(string search)
+        => BinarySearch.Search(TestSureNames, 0, TestSureNames.Count - 1, search).Should().Be(NotFoundIndex);
+}
